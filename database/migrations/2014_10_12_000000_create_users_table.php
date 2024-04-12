07e5270eeb19872052193058ec8890cadcd41935
navneetgehlotsst <148967647+navneetgehlotsst@users.noreply.github.com>
2024-04-12 12:58:32 +0530

    db migration changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
-            $table->string('last_name');
+            $table->string('last_name')->nullable();
             $table->string('full_name');
             $table->string('slug');
             $table->string('email')->unique()->nullable();
