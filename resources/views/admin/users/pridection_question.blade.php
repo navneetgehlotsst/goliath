54eb9701e03ec6a3f937058e7e839223bf97b656
navneetgehlotsst <148967647+navneetgehlotsst@users.noreply.github.com>
2024-05-13 11:42:27 +0530

    cron issue resolved

--- resources/views/admin/users/pridection_question.blade.php
+++ resources/views/admin/users/pridection_question.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container-fluid flex-grow-1 container-p-y">
     <h5 class="py-2 mb-2">
-        <span class="text-primary fw-light">Pridicted Question</span>
+        <span class="text-primary fw-light">Predicted Question</span>
     </h5>
     <div class="row mb-5">
         <div class="col-md-12 col-lg-12">
@@ -15,7 +15,7 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="table-responsive text-nowrap">
-                            <h4>Totat Correct Question :- {{$predictedData['correct_counts']}}</h4>
+                            <h4>Total Correct Question :- {{$predictedData['correct_counts']}}</h4>
                             <h5> Total Winning Amount :- @if ($predictedData['correct_counts'] >= 5) {{$predictedData['winning_amount']}} @else 0  @endif</h5>
                             <table class="table">
                                 <thead>
